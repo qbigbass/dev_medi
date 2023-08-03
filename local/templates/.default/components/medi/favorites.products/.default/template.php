8b0e90abca01eeb91bbc981697c7ff7727e3c73c
Radik.M <r.machmutov@astarus.com>
2023-08-03 10:10:32 +0400

    Добавил иконку избранных товаров в футер для адаптива.

--- local/templates/.default/components/medi/favorites.products/.default/template.php
+++ local/templates/.default/components/medi/favorites.products/.default/template.php
@@ -1,4 +1,4 @@
-<a href="/lk/?favorite" class="favorites_link <?if($arResult['CNT_ITEMS']>0):?>has_items<?endif;?>">
+<a href="/lk/?favorite" class="favorites_link <?if($arResult['CNT_ITEMS']>0):?>has_items<?endif;?> <?= (strpos($APPLICATION->GetCurDir(), 'lk/') ? 'active' : '') ?> <?= ($USER->IsAuthorized() ? 'authorized' : '') ?>">
     <span class="count">
         <?if($arResult['CNT_ITEMS']>0):?><?=$arResult['CNT_ITEMS']?><?endif;?>
     </span>
