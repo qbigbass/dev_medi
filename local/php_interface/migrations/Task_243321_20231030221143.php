a5839f5147284c1c28577328c8afa94b6754d14e
r.makhmutov <r.machmutov@astarus.com>
2023-10-30 23:21:45 +0400

    #243321 Пересоздал миграцию для нового св-ва. Переписал скрипт для разового заполнения св-в "Активная размерная характеристика" для С-Петербурга и др городов

---
+++ local/php_interface/migrations/Task_243321_20231030221143.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Task_243321_20231030221143 extends Version
+{
+    protected $description = "Новое св-во \"Активная размерная характеристика СПБ\" в ИБ \"Пакет предложений\"";
+
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('offers', 'catalog');
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Активная размерная характеристика СПБ',
+  'ACTIVE' => 'Y',
+  'SORT' => '10000',
+  'CODE' => 'SELECTED_SIZE_CHARACT_SPB',
+  'DEFAULT_VALUE' => '',
+  'PROPERTY_TYPE' => 'L',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'C',
+  'MULTIPLE' => 'N',
+  'XML_ID' => '',
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => NULL,
+  'USER_TYPE_SETTINGS' => NULL,
+  'HINT' => '',
+  'VALUES' => 
+  array (
+    0 => 
+    array (
+      'VALUE' => 'Y',
+      'DEF' => 'N',
+      'SORT' => '500',
+      'XML_ID' => 'Y',
+    ),
+  ),
+  'FEATURES' => 
+  array (
+    0 => 
+    array (
+      'MODULE_ID' => 'catalog',
+      'FEATURE_ID' => 'IN_BASKET',
+      'IS_ENABLED' => 'N',
+    ),
+    1 => 
+    array (
+      'MODULE_ID' => 'catalog',
+      'FEATURE_ID' => 'OFFER_TREE',
+      'IS_ENABLED' => 'N',
+    ),
+    2 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'DETAIL_PAGE_SHOW',
+      'IS_ENABLED' => 'N',
+    ),
+    3 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'LIST_PAGE_SHOW',
+      'IS_ENABLED' => 'N',
+    ),
+    4 => 
+    array (
+      'MODULE_ID' => 'yandex.market',
+      'FEATURE_ID' => 'YAMARKET_COMMON',
+      'IS_ENABLED' => 'N',
+    ),
+    5 => 
+    array (
+      'MODULE_ID' => 'yandex.market',
+      'FEATURE_ID' => 'YAMARKET_TURBO',
+      'IS_ENABLED' => 'N',
+    ),
+  ),
+));
+    
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
