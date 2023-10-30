2074f237f82f0e0474637f213d94967f57793582
r.makhmutov <r.machmutov@astarus.com>
2023-10-30 23:21:45 +0400

    #243321 Пересоздал миграцию для нового св-ва. Переписал скрипт для разового заполнения св-в "Активная размерная характеристика" для С-Петербурга и др городов

--- local/tools/set_selected_size_offers.php
+++ local/tools/set_selected_size_offers.php
@@ -1,5 +1,9 @@
 <?php
-/* Скрипт для разового заполнения св-ва "Активная размерная характеристика" у всех ТП */
+
+/**
+ * Скрипт для разового заполнения св-ва "Активная размерная характеристика" и
+ * "Активная размерная характеристика СПБ" у всех ТП
+ */
 define("NO_KEEP_STATISTIC", true);
 define("NOT_CHECK_PERMISSIONS",true);
 
@@ -51,9 +55,13 @@ if (!empty($arrOffersProducts)) {
 
     $GLOBALS["NOT_RUN_UPDATE_SELECTED_SIZE_OFFER"] = true; // Блокируем запуск обработчика события OnBeforeIBlockElementUpdate с функцией UpdateSelectedSizeOffer
 
+    $arrOffers = []; // Найденные ТП
+    $arrProductIdsSkuIds = []; // Массив товаров с Ids ТП
     // Отсортируем ТП по полю "Сортировка"
-    foreach ($arrOffersProducts as $productId => $arrOffers) {
-        foreach ($arrOffers as $offerId => $offerData) {
+    foreach ($arrOffersProducts as $productId => $arrSkuIds) {
+        foreach ($arrSkuIds as $offerId => $offerData) {
+            $arrOffers[] = $offerId;
+            $arrProductIdsSkuIds[$productId][] = $offerId;
             $arrOffersProductsSort[$productId][$offerData["SORT"]] = $offerId;
         }
         ksort($arrOffersProductsSort[$productId]);
@@ -65,6 +73,170 @@ if (!empty($arrOffersProducts)) {
             "SELECTED_SIZE_CHARACT" => 16684
         ];
 
+        // Заполняем св-во "Активная размерная характеристика" (Для Москвы и др городов кроме Санкт Петербурга)
         CIBlockElement::SetPropertyValuesEx($id, "19", $propertyValues);
+    }
+
+    // Логика заполнения св-ва "Активная размерная характеристика СПБ" (Для г. Санкт Петербург)
+    // Найдем все подразделы раздела "Ортопедическая обувь" (ID=88)
+    $arrSubSections = getSubSectionsSection(88);
+    // Найдем разделы к которым принадлежат ранее полученные товары
+    $arrElemGroupSections = getGroupsElements($arrProducts);
+    // Проверим принадлежность каждого товара к разделу "Ортопедическая обувь"
+    // Найдем товары которые принадлежат разделу "Ортопедическая обувь"
+    $arrProductsShoes = []; // Продукты из раздела "Ортопедическая обувь"
+    if (!empty($arrElemGroupSections)) {
+        foreach ($arrElemGroupSections as $productId => $arrSections) {
+            foreach ($arrSections as $sectionId) {
+                if (in_array($sectionId, $arrSubSections)) {
+                    $arrProductsShoes[] = $productId;
+                    break;
+                }
+            }
+        }
+    }
+
+    $arrProductsNotShoes = array_diff($arrProducts, $arrProductsShoes); // Товары из других разделов и товары которые не имеют предложений
+    $arrOffersShoes = []; // ТП принадлежавшие разделу "Ортопедическая обувь"
+    if (!empty($arrProductsShoes)) {
+        foreach ($arrProductsShoes as $productId) {
+            if (array_key_exists($productId, $arrProductIdsSkuIds)) {
+                $arrOffersShoes = array_merge($arrProductIdsSkuIds[$productId], $arrOffersShoes);
+            }
+        }
+    }
+
+    $arrOffersNotShoes = array_diff($arrOffers, $arrOffersShoes); // ТП из других разделов
+
+    // Проверим наличие всех ТП на складах
+    $arrStoreOfferShoes = [];
+    $arrStoreOfferNotShoes = [];
+
+    if (!empty($arrOffersShoes)) {
+        // Проверим наличие всех ТП из раздела "Ортопедическая обувь" на складах в г.Санкт-Петербург
+        $filter = [
+            "ACTIVE" => "Y",
+            "PRODUCT_ID" => $arrOffersShoes,
+            "SITE_ID" => "s2",
+            [
+                "LOGIC" => "OR",
+                ["UF_STORE" => true],
+                ["UF_SHOES_STORE" => true]
+            ]
+        ];
+
+        $rsProps = CCatalogStore::GetList(
+            array('TITLE' => 'ASC', 'ID' => 'ASC'),
+            $filter,
+            false,
+            false,
+            ["ID", "ACTIVE", "ELEMENT_ID", "PRODUCT_AMOUNT", "UF_STORE", "SITE_ID"]
+        );
+
+        while ($mStore = $rsProps->GetNext()) {
+            $arrStoreOfferShoes[$mStore['ELEMENT_ID']] += $mStore['PRODUCT_AMOUNT'];
+        }
+    }
+
+    unset($filter);
+
+    if (!empty($arrOffersNotShoes)) {
+        // Проверим наличие всех ТП из других разделов на складах в г.Санкт-Петербург
+        $filter = [
+            "ACTIVE" => "Y",
+            "PRODUCT_ID" => $arrOffersNotShoes,
+            "SITE_ID" => "s2",
+            "UF_STORE" => true,
+        ];
+
+        $rsProps = CCatalogStore::GetList(
+            array('TITLE' => 'ASC', 'ID' => 'ASC'),
+            $filter,
+            false,
+            false,
+            ["ID", "ACTIVE", "ELEMENT_ID", "PRODUCT_AMOUNT", "UF_STORE", "SITE_ID"]
+        );
+
+        while ($mStore = $rsProps->GetNext()) {
+            $arrStoreOfferNotShoes[$mStore['ELEMENT_ID']] += $mStore['PRODUCT_AMOUNT'];
+        }
+    }
+
+    // исключить онлайн продажу, только бронь в салоне
+    $exceptionOffers = ["41078", "41079", "41080", "41081", "41082", "41083", "41084", "41085", "41086"];
+    // Обнулим остаток на складе для ТП попадающих в исключения
+
+    $arrOfferIdsAmount = []; // Итоговый массив ТП с остатками по складам в г. Санкт-Перербург
+    if (!empty($arrStoreOfferShoes)) {
+        foreach ($arrStoreOfferShoes as $offerId => $storeAmountShoes) {
+            if (in_array($offerId, $exceptionOffers)) {
+                $arrOfferIdsAmount[$offerId] = 0;
+            } else {
+                $arrOfferIdsAmount[$offerId] = $storeAmountShoes;
+            }
+        }
+    }
+
+    if (!empty($arrStoreOfferNotShoes)) {
+        foreach ($arrStoreOfferNotShoes as $offerId => $storeAmountNotShoes) {
+            if (in_array($offerId, $exceptionOffers)) {
+                $arrOfferIdsAmount[$offerId] = 0;
+            } else {
+                $arrOfferIdsAmount[$offerId] = $storeAmountNotShoes;
+            }
+        }
+    }
+
+    // Добавим результаты по складам в итоговый массив
+    foreach ($arrOffersProducts as $productId => $arrSkuIds) {
+        foreach ($arrSkuIds as $offerId => $offerData) {
+            $arrOffersProducts[$productId][$offerId]["AMOUNT"] = $arrOfferIdsAmount[$offerId];
+        }
+    }
+
+    // Отсортируем ТП у каждого товара по полю "Сортировка"
+    unset($arrSkuIds);
+    $arrOffersProductsSort = [];
+    $arrOffersUpdated = [];
+    foreach ($arrOffersProducts as $productId => $arrSkuIds) {
+        foreach ($arrSkuIds as $offerId => $offerData) {
+            $arrOffersProductsSort[$productId][$offerData["SORT"]][$offerId] = $offerData["AMOUNT"];
+        }
+        ksort($arrOffersProductsSort[$productId]);
+    }
+
+    // Найдем для каждого товара только одно ТП для установки активной размерной характеристики для г. Санкт-Петербург
+    if (!empty($arrOffersProductsSort)) {
+        foreach ($arrOffersProductsSort as $productId => $arrSort) {
+            foreach ($arrSort as $sortId => $arrSkuIds) {
+                foreach ($arrSkuIds as $offerId => $amount) {
+                    if ($amount > 0) {
+                        $arrOffersUpdated[$productId] = $offerId;
+                    }
+                }
+            }
+
+            if (empty($arrOffersUpdated[$productId])) {
+                // У всех ТП внутри продукта отсутствуют остатки
+                // Выберем активную размерную размерную характеристику по полю "Сортировка"
+                $firstSortOffer = array_key_first($arrOffersProductsSort[$productId]);
+                $activeOfferId = array_key_first($arrOffersProductsSort[$productId][$firstSortOffer]);
+
+                if ($activeOfferId > 0) {
+                    $arrOffersUpdated[$productId] = $activeOfferId;
+                }
+            }
+        }
+    }
+
+    if (!empty($arrOffersUpdated)) {
+        foreach ($arrOffersUpdated as $productId => $skuId) {
+            $propertyValues = [
+                "SELECTED_SIZE_CHARACT_SPB" => 16685
+            ];
+
+            // Заполняем св-во "Активная размерная характеристика СБП" (Для г. Санкт Петербург)
+            CIBlockElement::SetPropertyValuesEx($skuId, "19", $propertyValues);
+        }
     }
 }
